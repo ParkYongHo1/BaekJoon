23c456bab243005c4a896ad7f1a3de209d657f05
ParkYongHo1 <qkaxhf8823@naver.com>
2024-07-02 17:54:26 +0900

    2024-07-02

---
+++ Bronze/1. [2588] 곱셈.py
@@ -0,0 +1,24 @@
+
+import sys
+# 입력받기
+a = int(sys.stdin.readline())
+b = int(sys.stdin.readline())
+
+first = a * (b % 10)   # 일의 자리 구해서 a랑 곱하기
+second = a * ((b%100)//10) # 십의 자리 구해서 a랑 곱하기
+third = a * (b//100) # 백의 자리 구해서 a랑 곱하기
+fourth = a * b
+
+# sys.stdout.write() -> 속도가 빨라짐
+sys.stdout.write(str(first)+'\n')
+sys.stdout.write(str(second )+ '\n')
+sys.stdout.write(str(third) + '\n')
+sys.stdout.write(str(fourth) +'\n')
+
+# TEST
+# a = 472
+# b = 385
+# first = 2360
+# second = 3776
+# third = 1416
+# fourth = 181720
