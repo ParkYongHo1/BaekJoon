1e380da0ba39fe699ea9f95444c26dff0b42a78f
YONGHO <103355252+ParkYongHo1@users.noreply.github.com>
2025-03-10 09:53:02 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 35.52 ms, Memory: 38.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -11,17 +11,17 @@ function solution(maps) {
         checkArray[row][col] = 1;
         while(queue.length){
             const [x , y] = queue.shift();
-            if(x === M - 1 && y === N - 1) return checkArray[x][y];
+           
             for(let i=0; i < 4; i++){
                 const [nx, ny] = [x + dx[i], y + dy[i]];
-                if(nx<0 || ny<0 || nx>=M || ny>=N) continue;
-                if(maps[nx][ny] && !checkArray[nx][ny]){
+                if((nx >= 0 && ny >= 0 && nx < M && ny < N ) && maps[nx][ny] && !checkArray[nx][ny]){
                     checkArray[nx][ny] = checkArray[x][y] + 1;
                     queue.push([nx, ny]);
                 }
             }
         }
-        return -1;
+  
     }
-    return bfs(0,0);
+   bfs(0,0);
+   return  checkArray[M -1][N -1] === 0 ? -1 : checkArray[M -1][N -1]
 }
