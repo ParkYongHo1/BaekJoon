8ab1f5ca37c5bc9d27b00335a472b21692d27c29
YONGHO <103355252+ParkYongHo1@users.noreply.github.com>
2025-04-27 13:57:04 +0900

    [level 2] Title: 전화번호 목록, Time: 287.51 ms, Memory: 93.4 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,15 +1,15 @@
 function solution(phone_book) {
-  let hashMap = new Map();
-  for(let i = 0; i < phone_book.length; i++){
-      hashMap.set(phone_book[i], true);
-  }
-  for(let i = 0; i < phone_book.length; i++){
-      for(let j = 0; j < phone_book[i].length; j++){
-          let prefix = phone_book[i].substring(0,j);
-          if(hashMap.has(prefix)){
-              return false;
-          }
-      }
-  }
-  return true;
+    let hashMap = new Map();
+    for(let number of phone_book){
+        hashMap.set(number,number);
+    }
+    for(let i = 0; i < phone_book.length; i++){
+        for(let j = 0; j < phone_book[i].length; j++){
+            let subStr = phone_book[i].substr(0,j);
+            if(hashMap.has(subStr)){
+                return false;
+            }
+        }
+    }
+    return true;
 }
