8683ca326c073cc4595cd28855f3dab4384b0d0c
YONGHO <103355252+ParkYongHo1@users.noreply.github.com>
2025-04-27 13:22:21 +0900

    [level 1] Title: 폰켓몬, Time: 0.56 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,10 +1,8 @@
 function solution(nums) {
-  let maxNum = nums.length / 2;
-  let setNum = new Set(nums);
-  let numArr = Array.from(setNum);
-  return maxNum == numArr.length
-    ? numArr.length
-    : maxNum < numArr.length
-    ? maxNum
-    : numArr.length
+    let maxNum = nums.length / 2;
+    
+    let hashSet = new Set(nums);
+    let answer = hashSet.size >= maxNum ?  maxNum :  hashSet.size;
+    
+    return answer
 }
