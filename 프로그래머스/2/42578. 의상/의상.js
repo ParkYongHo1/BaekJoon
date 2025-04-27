85baa101c7bcb49ed59a64854b5bc1eb85101004
YONGHO <103355252+ParkYongHo1@users.noreply.github.com>
2025-04-27 14:27:29 +0900

    [level 2] Title: 의상, Time: 0.10 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,11 +1,14 @@
 function solution(clothes) {
     let hashMap = new Map();
     let answer = 1;
-    for(let i = 0; i < clothes.length; i++){
-        hashMap.set(clothes[i][1], (hashMap.get(clothes[i][1]) || 0) + 1);
-    }
+    
+    clothes.forEach((item)=>{
+        hashMap.set(item[1], (hashMap.get(item[1]) || 0 ) + 1);    
+    })
+    
     for(let item of hashMap.values()){
-        answer *= (item + 1);
+        answer *= (item + 1)
     }
+    
     return answer - 1;
 }
