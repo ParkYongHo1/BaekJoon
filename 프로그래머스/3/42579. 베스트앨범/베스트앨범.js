00da204c260ff341a12582b2f294462a8df6987a
YONGHO <103355252+ParkYongHo1@users.noreply.github.com>
2025-04-28 09:41:03 +0900

    [level 3] Title: 베스트앨범, Time: 0.62 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -1,24 +1,28 @@
 function solution(genres, plays) {
-  let answer = [];
-  let hashMap = genres.reduce((acc, cur, index) => {
-    if (!acc[cur]) {
-      acc[cur] = { play: plays[index], list: [] };
-    } else {
-      acc[cur].play += plays[index];
-    }
-    acc[cur].list.push([index, plays[index]]);
-    return acc;
-  }, {});
-  let sortArr = Object.entries(hashMap).sort((a, b) => b[1].play - a[1].play);
-  sortArr.map((item, inex) => {
-    item[1].list.sort((a, b) => b[1] - a[1]);
+  const result = [];
+  const obj = {}; 
+  const info = {}; 
+
+  for (let i = 0; i < genres.length; i++) {
+    obj[genres[i]] = (obj[genres[i]] || 0) + plays[i];
+    info[i] = { play: plays[i], genre: genres[i] };
+  } 
+
+
+  const sortedGenre = Object.keys(obj).sort((a, b) => obj[b] - obj[a]);
+
+  sortedGenre.forEach((genre) => {
+
+    const genreSong = Object.values(info)
+      .filter((song) => song.genre === genre)
+      .sort((a, b) => b.play - a.play)
+      .slice(0, 2);
+
+    genreSong.forEach((gs) => {
+      const uniqueId = Object.values(info).indexOf(gs);
+      result.push(uniqueId);
+    });
   });
 
-  for (let i = 0; i < sortArr.length; i++) {
-    for (let j = 0; j < Math.min(2, sortArr[i][1].list.length); j++) {
-      answer.push(sortArr[i][1].list[j][0]);
-    }
-  }
-
-  return answer;
+  return result;
 }
