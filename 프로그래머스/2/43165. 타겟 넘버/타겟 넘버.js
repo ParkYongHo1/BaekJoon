500e4734f85da2ee15c500ad38d7cbbcaee0d55f
YONGHO <103355252+ParkYongHo1@users.noreply.github.com>
2026-01-18 20:34:58 +0900

    [level 2] Title: 타겟 넘버, Time: 14.35 ms, Memory: 35.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,15 +1,13 @@
 function solution(numbers, target) {
-    let answer =0;
-    dfs(0,0);
-    function dfs (index,sum){
+    let answer = 0;
+    const dfs = (index,sum) =>{
         if(index === numbers.length){
-            if(sum === target){
-                answer++;
-            } 
+            if(sum === target) answer++;
             return;
-        }    
-        dfs(index +1, sum + numbers[index])
-        dfs(index+1, sum - numbers[index])        
+        }
+        dfs(index + 1, sum + numbers[index])
+        dfs(index + 1, sum - numbers[index])
     }
+    dfs(0,0)
     return answer;
 }
